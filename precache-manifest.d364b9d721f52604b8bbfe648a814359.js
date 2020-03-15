d0258047344ae88165307037b7354e3cc70e2b9d
Karen Sargsyan <ksarg9618@gmail.com>
2020-03-15 21:04:29 +0400

    Updates

---
+++ precache-manifest.d364b9d721f52604b8bbfe648a814359.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c476e4fe96ebb2457c281ac45b2425ec",
+    "url": "/life-test/index.html"
+  },
+  {
+    "revision": "1b951353b7026f1ab854",
+    "url": "/life-test/static/js/2.72607162.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/life-test/static/js/2.72607162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfcbc4214122212c2bc2",
+    "url": "/life-test/static/js/main.71d4ea8c.chunk.js"
+  },
+  {
+    "revision": "ea3edf43e917335f9e64",
+    "url": "/life-test/static/js/runtime-main.61b118b5.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/life-test/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "fce5a65e4541fcef305da29566fa365d",
+    "url": "/life-test/static/media/feature1.fce5a65e.jpg"
+  },
+  {
+    "revision": "d1389b1a0c5fe74f40f83bcee244c601",
+    "url": "/life-test/static/media/feature2.d1389b1a.jpg"
+  },
+  {
+    "revision": "085b2d765961e476afd53e2accb4af72",
+    "url": "/life-test/static/media/feature3.085b2d76.jpg"
+  }
+]);
